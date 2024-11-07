4e285d6b66000d6a2dcab16deaa2cbb3602cdca6
nicholusmahlangu1 <nicholus.mahlangu@nlsa.ac.za>
2024-11-07 11:27:49 +0200

    We added admin-dashboard, admin-login page, cataloguer-login page, signup page into the current version of the E-pubs system

--- assets/php/Login.php
+++ assets/php/Login.php
@@ -2,7 +2,7 @@
 // Start session
 session_start();
 // Database connection parameters
-include '../assets/php/conn.php';
+include 'conn.php';
 
 // // Create connection
 // $conn = new mysqli($servername, $username, $password, $database);
@@ -11,24 +11,24 @@ include '../assets/php/conn.php';
 // if ($conn->connect_error) {
 //     die("Connection failed: " . $conn->connect_error);
 // }
-
+$username = $_POST['email'];
+$password = $_POST['password'];
 // Check if the form is submitted
-if (isset($_POST['email']) && isset($_POST['password'])) {
+if (trim($username)!=""and trim($password)!= "") {
     // Retrieve form data
     echo"it reaches this point";
-    $email = $_POST['email'];
-    $password = $_POST['password'];
+
 
     // Perform SQL query to check user credentials
-    $sql = "SELECT * FROM users WHERE EmailAddress = '$email' AND Password = '$password'";
+    $sql = "SELECT * FROM users WHERE EmailAddress = '$username' AND Password = '$password'";
     $result = $conn->query($sql);
     
     // Check if user exists
     if ($result->num_rows > 0) {
         // User authenticated successfully, set session variables
-        $_SESSION['email'] = $email;
+        $_SESSION['email'] = $username;
         // Redirect to dashboard or desired page
-        header("Location: index.html");
+        header("Location:../../application files/index.html");
         exit();
     } else {
         // Invalid credentials, display error message
---
+++ assets/php/adminlogin.php
@@ -0,0 +1,38 @@
+<?php
+// Start session
+session_start();
+// Database connection parameters
+include 'conn.php';
+
+// // Create connection
+// $conn = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+// if ($conn->connect_error) {
+//     die("Connection failed: " . $conn->connect_error);
+// }
+$username = $_POST['email'];
+$password = $_POST['password'];
+// Check if the form is submitted
+if (trim($username)!=""and trim($password)!= "") {
+    // Retrieve form data
+ // Perform SQL query to check user credentials
+    $sql = "SELECT * FROM admin WHERE username = '$username' AND Password = '$password'";
+    $result = $conn->query($sql);
+    
+    // Check if user exists
+    if ($result->num_rows > 0) {
+        // User authenticated successfully, set session variables
+        $_SESSION['email'] = $username;
+        // Redirect to dashboard or desired page
+        header("Location:../../application files/admin_dashboard.html");
+        exit();
+    } else {
+        // Invalid credentials, display error message
+        echo "Invalid email or password.";
+    }
+}
+
+// Close connection
+$conn->close();
+?>
