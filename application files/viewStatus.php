ebdfb17ede3449890b53e792b1777d2fe4c95142
nicholusmahlangu <nicholus.mahlangu@nlsa.ac.za>
2025-01-27 10:13:57 +0200

    27-1-2025 -> I was adding the NLSA tab icon on all pages, so that we can have uniformity on all the pages.

--- application files/viewStatus.php
+++ application files/viewStatus.php
@@ -58,6 +58,7 @@ $totalPages = ceil($totalRecords / $limit);
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Status Management</title>
+  <link href="../assets/img/favicon.webp" rel="icon">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
   <style>
